978307e0fb8c2d093dc94a5d50d1c6ba6f9010b4
Stefano Chiodino <stefano.chiodino@epiphanysolutions.co.uk>
2017-09-13 14:14:01 +0100

    ResettableLazy. v1.0.5

---
+++ Epiphany.Core/Auto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Security.Permissions;
+using System.Threading;
+
+namespace Epiphany.Core
+{
+    [HostProtection(Action = SecurityAction.LinkDemand, Resources = HostProtectionResource.Synchronization | HostProtectionResource.SharedState)]
+    public class Auto<T> where T : class
+    {
+        public Auto(Func<T> create, LazyThreadSafetyMode safetyMode = LazyThreadSafetyMode.ExecutionAndPublication)
+        {
+            _syncRoot = new object();
+            ThreadSafety = safetyMode;
+            Create = create;
+        }
+
+        protected LazyThreadSafetyMode ThreadSafety { get; private set; }
+        protected Func<T> Create { get; private set; }
+        private readonly object _syncRoot;
+
+        public T Value
+        {
+            get
+            {
+                if (_value == null)
+                {
+                    switch (ThreadSafety)
+                    {
+                        case LazyThreadSafetyMode.PublicationOnly:
+                            var value = Create();
+                            lock (_syncRoot)
+                            {
+                                if (_value == null)
+                                {
+                                    _value = value;
+                                }
+                            }
+                            break;
+
+                        case LazyThreadSafetyMode.ExecutionAndPublication:
+                            lock (_syncRoot)
+                            {
+                                if (_value == null)
+                                {
+                                    _value = Create();
+                                }
+                            }
+                            break;
+
+                        default:
+                            _value = Create();
+                            break;
+                    }
+                }
+                return _value;
+            }
+        }
+
+        private T _value;
+
+        public bool IsValueCreated
+        {
+            get { return _value != null; }
+        }
+
+        public void ResetValue()
+        {
+            if (ThreadSafety != LazyThreadSafetyMode.None)
+            {
+                lock (_syncRoot)
+                {
+                    _value = null;
+                }
+            }
+            else
+            {
+                _value = null;
+            }
+        }
+    }
+}
---
+++ Epiphany.Core/ResettableLazy.cs
@@ -0,0 +1,20 @@
+﻿namespace Epiphany.Core
+{
+    public abstract class ResettableLazy<T> where T : class, new()
+    {
+        protected static Auto<T> Lazy { get; private set; }
+
+        static ResettableLazy()
+        {
+            Lazy = new Auto<T>(() => new T());
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                return Lazy.Value;
+            }
+        }
+    }
+}
